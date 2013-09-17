315be22f64dd4a7d06835850d1bb5094c9dee22a
Cristóbal Caballero <cristobal.caballero@gmx.de>
2013-09-17 21:41:35 +0200

    Maven commit

--- src/main/java/es/cristobal/test_javadocs_maven.java
+++ src/main/java/es/cristobal/test_javadocs_maven.java
@@ -1,10 +1,19 @@
 package es.cristobal;
 
+public class test_javadocs_maven 
+{
 
-import org.apache.maven.plugin.*;
-
-public class test_javadocs_maven {
-
-	org.apache.maven.plugin.github
+	public test_javadocs_maven()
+	{
+		
+		return;
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hola Maven con Git!");
+		
+		return;
+	}
 	
 }
